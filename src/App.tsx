9113ddcd7aa03a2119c0c627bb7b322a6510dccc
fernandocastilloguardiajose-dot <fernandocastilloguardiajose@gmail.com>
2026-04-17 19:23:03 -0600

    Initial commit: Luxury Urban Clothing website

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Checkout from './pages/Checkout';
+
+function App() {
+  return (
+    <CartProvider>
+      <div className="min-h-screen bg-black text-white selection:bg-gold/30">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </CartProvider>
+  );
+}
+
+export default App;
